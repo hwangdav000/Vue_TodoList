3da12d94635006e47a57c43b5e6a5785bd924558
hwan0259 <31775675+hwan0259@users.noreply.github.com>
2020-07-26 15:39:42 -0500

    pushing dist

---
+++ dist/js/about.02e7b2ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About")]),s("p",[t._v(" This is the Todolist app v1.0 It is part of vue crash course ")])])}],u=s("2877"),o={},r=Object(u["a"])(o,n,a,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.02e7b2ce.js.map
